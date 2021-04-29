42e56ffc13e7a6951828f7aca915a6e33f49cc55
sairamsnv <55974736+sairamsnv@users.noreply.github.com>
2021-04-29 14:06:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask,render_template
 import pandas as pd
+
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
+
+
+
 def hello():
-    df = pd.read_excel("C:/Users/laxmi/jonathanY.Xlsx")
+    df = pd.read_csv(r'https://raw.githubusercontent.com/sairamsnv/jsonapi/main/jonathanY.csv')
     dj = df.to_json()
     return render_template('hello.html',si='{}'.format(dj))
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
-
